43bcc58332c4cdc0e7245b3e5ec5b66e325b172e
HUSSTECH <huss@husstechlabs.com>
2016-08-29 03:06:51 +0100

    added day countdown response
    day countdown response
    update help
    update desc

--- bot/messenger.py
+++ bot/messenger.py
@@ -1,5 +1,7 @@
 import logging
 import random
+import re
+from datetime import datetime,date
 
 logger = logging.getLogger(__name__)
 
@@ -22,7 +24,8 @@ class Messenger(object):
             "I'm your friendly Slack bot written in Python.  I'll *_respond_* to the following commands:",
             "> `hi <@" + bot_uid + ">` - I'll respond with a randomized greeting mentioning your user. :wave:",
             "> `<@" + bot_uid + "> joke` - I'll tell you one of my finest jokes, with a typing pause for effect. :laughing:",
-            "> `<@" + bot_uid + "> attachment` - I'll demo a post with an attachment using the Web API. :paperclip:")
+            "> `<@" + bot_uid + "> attachment` - I'll demo a post with an attachment using the Web API. :paperclip:",
+            "> `<@" + bot_uid + "> countdown yyyy-mm-dd` - I'll give you a day countdown to that date (UK format obvs :uk:)")
         self.send_message(channel_id, txt)
 
     def write_greeting(self, channel_id, user_id):
@@ -59,3 +62,11 @@ class Messenger(object):
             "color": "#7CD197",
         }
         self.clients.web.chat.post_message(channel_id, txt, attachments=[attachment], as_user='true')
+
+    def day_countdown(self, channel_id, input_text):
+        date_str = re.search("\d{4}[-]\d{2}[-]\d{2}",input_text).group()
+        from_date = date.today()
+        to_date = datetime.strptime(date_str,'%Y-%m-%d')
+        days = abs((to_date-from_date).days)
+        txt = ":clock1: only *"+days+"* until "+date_str+"!!! :thumbsup: :sparkles: :boom: :tada:"
+        self.send_message(channel_id, txt)
